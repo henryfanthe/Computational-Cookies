e67b99862d0c3678a808762768124834687181d3
henryfanthe <58746840+henryfanthe@users.noreply.github.com>
2019-12-10 13:48:36 -0500

    Add files via upload

--- Final Project/src/Cookie.java
+++ Final Project/src/Cookie.java
@@ -1,4 +1,23 @@
 
 public class Cookie {
+	private int xlocation;
+	private int ylocation;
 
+		public Cookie() {
+			xlocation = 0;
+			ylocation = 0;
+		}
+		
+		public Cookie(int x, int y) {
+			xlocation = x;
+			ylocation = y;
+		}
+		
+		public int getX() {
+			return xlocation;
+		}
+		
+		public int getY() {
+			return ylocation;
+		}
 }
