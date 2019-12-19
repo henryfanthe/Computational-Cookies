cec19ba85f6c8565c768102ef8968f8b557e8955
henryfanthe <58746840+henryfanthe@users.noreply.github.com>
2019-12-19 15:00:17 -0500

    Add files via upload

--- Board.java
+++ Board.java
@@ -1,6 +1,8 @@
 
 public class Board {
-
+	static int[][] temp = new int[5][5];
+	static boolean goAgain = true;
+	static int counter = 0;
 	/**
 	 * Random Number Generator
 	 * PreCon: none
@@ -32,46 +34,31 @@ public class Board {
 		}
 		System.out.println("");
 		
-		System.out.println("Horizontal");
-		board = CheckPieces.Horizontal(board);
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[0].length; j++) {
-				System.out.print(board[i][j] + " ");
+			temp = CheckPieces.Horizontal(board);
+			for (int i = 0; i < temp.length; i++) {
+				for (int j = 0; j < temp[0].length; j++) {
+					if (temp[i][j] != board[i][j]) {
+						board = replaceBoard(temp, board);
+						break;
+					}
+				}
 			}
-			System.out.println("");
 		}
-		System.out.println("");
 		
-		System.out.println("Vertical");
-		board = CheckPieces.Vertical(board);
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[0].length; j++) {
-				System.out.print(board[i][j] + " ");
+		for (int i = 0; i < temp.length; i++) {
+			for (int j = 0; j < temp[0].length; j++) {
+				System.out.print(board[i][j]);
+				}
 			}
-			System.out.println("");
-		}
-		System.out.println("");
-		
-		System.out.println("Left Diagonal");
-		board = CheckPieces.LeftDiagonal(board);
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[0].length; j++) {
-				System.out.print(board[i][j] + " ");
-			}
-			System.out.println("");
-		}
-		System.out.println("");
-		
-		System.out.println("Right Diagonal");
-		board = CheckPieces.RightDiagonal(board);
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[0].length; j++) {
-				System.out.print(board[i][j] + " ");
-			}
-			System.out.println("");
-		}
-		System.out.println("");
-			
+		System.out.println(" ");
 	}
-	
-}
+	public static int[][] replaceBoard(int[][] temp, int[][] board) {
+		for (int i = 0; i < temp.length; i++) {
+			for (int j = 0; j < temp[0].length; j++) {
+				board[i][j] = temp[i][j];
+				}
+			}
+		return board;
+		}
+	}
+
