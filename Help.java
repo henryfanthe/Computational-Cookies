9742580545fd22b1ce7048e29ec44938aa1bc1e8
henryfanthe <58746840+henryfanthe@users.noreply.github.com>
2020-01-12 14:33:02 -0500

    Add files via upload

--- Help.java
+++ Help.java
@@ -1,53 +1,70 @@
+/*
+ * Name: Henry
+ * Date: Jan 11, 2020
+ * This program displays basic help info for how to play the game
+ */
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-public class Help extends JPanel implements ActionListener {
-	private JButton back = new JButton("Back");
+
+
+public class Help extends JPanel {
+	//labels that tell the use how to play
 	private JLabel title = new JLabel("Help");
 	private JLabel howToPlay = new JLabel("Click on one cookie then click on another next to it to change their places");
 	private JLabel howToPlay2 = new JLabel("Combine three in a row to clear them and get points");
-	private JLabel howToPlay3 = new JLabel("Get as many points as you want");
-	private static int call = 2;
+	private JLabel howToPlay3 = new JLabel("Get as many points as you can");
 	
+	private JLabel PinaColadalb1 = new JLabel("Click on a type of cookie to get rid of");
+	private JLabel PinaColadalb2 = new JLabel("Click on the Pina Colada Button");
+	
+	private JLabel RamtinPoplb1 = new JLabel("Click on a cookie that you want to get rid of");
+	private JLabel RamtinPoplb2 = new JLabel("Click on the Ramtin Pop Button");
+	//creates a variable for the background
+	private BufferedImage background;
+	
+	/*
+	 * Constructor
+	 * Pre: none
+	 * Post: Creates a JPanel that has text displayed with an image for a background.
+	 */
 	public Help() {
+		//sets the background
 		try {
-			BufferedImage background = ImageIO.read(new File("Game Start.png"));
+			background = ImageIO.read(new File("Help Screen.png"));
 		} catch (IOException e) {
 			System.out.print("Image Not Available");
 		}
-		setCall();
-		this.setLayout(new GridLayout(5, 1, 50, 50));
+		//sets the layouts to stak on top of each other.
+		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		//centers the text
+		title.setAlignmentX(CENTER_ALIGNMENT);
+		howToPlay.setAlignmentX(CENTER_ALIGNMENT);
+		howToPlay2.setAlignmentX(CENTER_ALIGNMENT);
+		howToPlay3.setAlignmentX(CENTER_ALIGNMENT);
+		PinaColadalb1.setAlignmentX(CENTER_ALIGNMENT);
+		PinaColadalb2.setAlignmentX(CENTER_ALIGNMENT);
+		RamtinPoplb1.setAlignmentX(CENTER_ALIGNMENT);
+		RamtinPoplb2.setAlignmentX(CENTER_ALIGNMENT);
+		//adds the text to the JFrame
 		this.add(title);
 		this.add(howToPlay);
 		this.add(howToPlay2);
 		this.add(howToPlay3);
-		this.add(back);
-		back.addActionListener(this);
-		
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == back) {
-			System.out.println("Back");
-			call = 0;
-		}
-	}
-	@Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(Color.blue);
-        
-    }
-	public static int getCall() {
-		return call;
+		this.add(PinaColadalb1);
+		this.add(PinaColadalb2);
+		this.add(RamtinPoplb1);
+		this.add(RamtinPoplb1);
 	}
 	
-	public void setCall() {
-		call = 2;
-	}
+@Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background, 0, 0, this);
+        
+    }
 }
---
+++ PinaColda.java
@@ -0,0 +1,26 @@
+/*
+ * Name: Henry
+ * Date: Jan 12, 2020
+ * Desc: This gets rid of every single instance of a type of cookie and makes the cookies above it to fall into place
+ */
+public class PinaColda {
+
+	/*
+	 * Method finds all instance of a cookie and gets rid of it
+	 * Pre: the type of cookie is specified
+	 * Post: returns a board with all instances of a cookie removed from where it was before
+	 */
+	public static Cookies[][] activate(Cookies[][] board, int type){
+		//nested loop to find every instance of a cookie
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				if (board[i][j].getType() == type) {
+					//replaces that single coookie found
+					CheckPieces.movePiecesSingle(i, j, board);
+				}
+			}
+		}
+		//returns new board
+		return board;
+	}
+}
